c31b9b854b8e943ee19f74b239ce6178ea2d4784
Litmus-Hotel <nelsonmoraro111@gmail.com>
2026-07-21 10:44:56 -0700

    Delete src directory

--- src/config/db.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-
-// A single shared Prisma Client instance for the whole app.
-const prisma = new PrismaClient();
-
-module.exports = { prisma };
